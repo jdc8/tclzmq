e710778197b821068cf0318fc5645a3fd9172d3b
Jos Decoster <jos.decoster@gmail.com>
2012-03-14 07:50:58 -0400

    more examples

---
+++ examples/syncpub.tcl
@@ -0,0 +1,41 @@
+#
+#  Synchronized publisher
+#
+
+package require tclzmq
+
+tclzmq::context context 1
+
+#  We wait for 10 subscribers
+set SUBSCRIBERS_EXPECTED  10
+
+# Socket to talk to clients
+tclzmq::socket publisher context PUB
+publisher bind "tcp://*:5561"
+
+# Socket to receive signals
+tclzmq::socket syncservice context REP
+syncservice bind "tcp://*:5562"
+
+# Get synchronization from subscribers
+puts "Waiting for subscribers"
+set subscribers 0
+while {$subscribers < $SUBSCRIBERS_EXPECTED} {
+    # - wait for synchronization request
+    syncservice s_recv
+    # - send synchronization reply
+    syncservice s_send ""
+    incr subscribers
+}
+
+# Now broadcast exactly 1M updates followed by END
+puts "Broadcasting messages"
+for {set update_nbr 0} {$update_nbr < 1000000} {incr update_nbr} {
+    publisher s_send "Rhubarb"
+}
+
+publisher s_send "END"
+
+publisher close
+syncservice close
+context term
