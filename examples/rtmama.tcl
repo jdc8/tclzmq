f3189ce36490a0ac6d0e32ad143a683a22bd7b93
jdc8 <jos.decoster@gmail.com>
2012-03-19 04:33:04 -0400

    reorganized rtmama example

--- examples/rtmama.tcl
+++ examples/rtmama.tcl
@@ -1,15 +1,91 @@
-if {[llength $argv] != 1} {
-    puts "Usage: rtmama.tcl <number_of_workers>"
+#
+# Custom routing Router to Mama (ROUTER to REQ)
+#
+
+package require tclzmq
+
+if {[llength $argv] == 0} {
+    set argv [list driver 3]
+} elseif {[llength $argv] != 2} {
+    puts "Usage: rtmama.tcl <driver|main|worker> <number_of_workers>"
     exit 1
 }
 
+lassign $argv what NBR_WORKERS
+
 set tclsh [info nameofexecutable]
 set nbr_of_workers [lindex $argv 0]
+expr {srand([pid])}
 
-puts "Start main"
-exec $tclsh rtmama_main.tcl $nbr_of_workers > main.log 2>@1 &
+switch -exact -- $what {
+    worker {
+	tclzmq::context context 1
 
-for {set i 0} {$i < $nbr_of_workers} {incr i} {
-    puts "Start worker $i"
-    exec $tclsh rtmama_worker.tcl > worker$i.log 2>@1 &
+	tclzmq::socket worker context REQ
+
+	# We use a string identity for ease here
+	set id [format "%04X-%04X" [expr {int(rand()*0x10000)}] [expr {int(rand()*0x10000)}]]
+	worker setsockopt IDENTITY $id
+	worker connect "ipc://routing.ipc"
+
+	set total 0
+	while {1} {
+	    # Tell the router we're ready for work
+	    worker s_send "ready"
+
+	    # Get workload from router, until finished
+	    set workload [worker s_recv]
+	    if {$workload eq "END"} {
+		puts "Processed: $total tasks"
+		break
+	    }
+	    incr total
+
+	    # Do some random work
+	    after [expr {int(rand()*1000)}]
+	}
+
+	worker close
+	context term
+    }
+    main {
+	tclzmq::context context 1
+
+	tclzmq::socket client context ROUTER
+	client bind "ipc://routing.ipc"
+
+	for {set task_nbr 0} {$task_nbr < $NBR_WORKERS * 10} {incr task_nbr} {
+	    # LRU worker is next waiting in queue
+	    set address [client s_recv]
+	    set empty [client s_recv]
+	    set ready [client s_recv]
+	    client s_sendmore $address
+	    client s_sendmore ""
+	    client s_send "This is the workload"
+	}
+
+	# Now ask mamas to shut down and report their results
+	for {set worker_nbr 0} {$worker_nbr < $NBR_WORKERS} {incr worker_nbr} {
+	    set address [client s_recv]
+	    set empty [client s_recv]
+	    set ready [client s_recv]
+	    client s_sendmore $address
+	    client s_sendmore ""
+	    client s_send "END"
+	}
+
+	client close
+	context term
+    }
+    driver {
+	puts "Start main, output redirected to main.log"
+	exec $tclsh rtmama.tcl main $NBR_WORKERS > main.log 2>@1 &
+
+	after 1000
+
+	for {set i 0} {$i < $NBR_WORKERS} {incr i} {
+	    puts "Start worker $i, output redirected to worker$i.log"
+	    exec $tclsh rtmama.tcl worker $NBR_WORKERS > worker$i.log 2>@1 &
+	}
+    }
 }
