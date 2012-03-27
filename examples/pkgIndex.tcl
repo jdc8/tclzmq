6ba2b6c85b312b73c0e5981b68260afaeaa6a615
jdc8 <jos.decoster@gmail.com>
2012-03-27 11:21:40 -0400

    put mdbroker example in one file

--- examples/pkgIndex.tcl
+++ examples/pkgIndex.tcl
@@ -1,4 +1,3 @@
 package ifneeded mdp      1.0 [list source [file join $dir mdp.tcl]]
-package ifneeded MDBroker 1.0 [list source [file join $dir mdbrkapi.tcl]]
 package ifneeded MDClient 1.0 [list source [file join $dir mdcliapi.tcl]]
 package ifneeded MDWorker 1.0 [list source [file join $dir mdwrkapi.tcl]]
