519ff155397e9c8019b50bd0e8c9fbe6faffe21b
jdc8 <jos.decoster@gmail.com>
2012-06-19 11:19:08 -0400

    refactor docs

--- doc/build.tcl
+++ doc/build.tcl
@@ -1,12 +1,8 @@
 package require doctools
 
 set on [doctools::new on -format html]
-set f [open zmq.man r]
-set txt [read $f]
-close $f
-
 set f [open zmq.html w]
-puts $f [$on format $txt]
+puts $f [$on format {[include zmq.man]}]
 close $f
 
 $on destroy
